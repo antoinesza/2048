5ae7e4f9fb41e62d084908fd7b4ae91f3f72afdd
antoine szajryck <antoine.szajryck@gmail.com>
2018-11-13 11:29:07 +0100

    update

---
+++ src/index/script.js
@@ -0,0 +1,13 @@
+
+
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContent("2d");
+
+var sizeInput = document.getElementById("size");
+var changeSize = document.getElementById("Change-size");
+var scoreLabel = document.getElementById("score");
+
+var score = 0;
+var size = 4;
+var width  = canvas.width / 
